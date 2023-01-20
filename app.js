7e3ea729dd564f819abfad07f8c9fa8f5f8d3fd5
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-20 14:56:54 -0800

    accessed data set on load

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 /* Imports */
-
+import { getBabies } from './fetch-utils.js';
 /* Get DOM Elements */
+const babyList = document.querySelector('baby-list');
+const selectionForm = document.querySelector('selection-form');
+const dropdown = document.querySelector('dropdown');
     //form and button
 /* State */
+let babies = [];
+let astroSigns = [];
 
 /* Events */
+// addEventListener on load
+window.addEventListener('load', async () => {
 
-// addEventListener on load 
+    const response = await getBabies();
+    babies = response;
+    console.log(response);
+// call findCountries function with no arguments to fetch all countries (Slice A);
+// Slice B: call asynchronous getContinents fetch function and set to response variable
+// Slice B: set the continents state to the response.data
+// Slice B: call displayContinentOptions function;
+
+});
+
 // addEventListener on Submit
+selectionForm.addEventListener('submit', () => {
+    // e.preventDefault();
+    // const formData = new FormData(selectionForm);
+    // // Slice C: Call findCountries with continent from formData
+    // const value = formData.get('astro-');
+    // findCountries(value);
+});
+
 
 /* Display Functions */
+function displayBabies() {
 
+}
+
+function displayAstroSigns() {
+
+}
 // (don't forget to call any display functions you want to run on page load!)
---
+++ fetch-utils.js
@@ -0,0 +1,19 @@
+const SUPABASE_URL = "https://gxwgjhfyrlwiqakdeamc.supabase.co";
+const SUPABASE_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA";
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+
+export async function getBabies() {
+    let { data, error } = await client
+        .from('beanie_babies')
+        .select('*');
+
+    return data;
+}
+
+
+
+
+// export async functions that fetch data
